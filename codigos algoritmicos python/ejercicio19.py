43a4b6a44f9f4f2bd098e33af7eaa87e4b51db06
Gabriel Jose <gabrieljfernandeza@gmail.com>
2026-05-11 17:40:25 -0500

    commit

--- codigos algoritmicos python/ejercicio19.py
+++ codigos algoritmicos python/ejercicio19.py
@@ -1,5 +1,18 @@
 print("mostrar impuestos de ingresos")
 salario= float(input("ingrese salario:  "))
+
 if(salario<1500000):
+        impuesto=0 
+        
+elif(salario<=3000000):
+        
+    impuesto=salario*0.10
+
+else:
+    impuesto=salario*0.20
     
-    
+neto=salario-impuesto
+
+print("el salario es: ",salario)
+print("el impuesto a pagar es: ",impuesto)
+print("el salario neto es: ",neto)
